9d6e329351ce4c72ca5746bac27d0031a6cf7e50
Imene-Amirat <imene2004.amirat@gmail.com>
2025-11-20 17:52:34 +0100

    Override the security configuration

--- src/main/java/com/example/atablevotingsystembackend/repository/UserRepository.java
+++ src/main/java/com/example/atablevotingsystembackend/repository/UserRepository.java
@@ -6,7 +6,11 @@ import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(@NotBlank @Email String email);
+
+    Optional<User> findByEmail(String username);
 }
--- src/main/java/com/example/atablevotingsystembackend/security/config/ApplicationConfig.java
+++ src/main/java/com/example/atablevotingsystembackend/security/config/ApplicationConfig.java
@@ -1,9 +1,15 @@
 package com.example.atablevotingsystembackend.security.config;
 
+import com.example.atablevotingsystembackend.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 /**
@@ -11,8 +17,35 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * Déclarer le AuthenticationManager / AuthenticationProvider qui utilise ton UserDetailsService
 **/
 public class ApplicationConfig {
+    private final UserRepository userRepository;
+
+    public ApplicationConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
     }
 }
